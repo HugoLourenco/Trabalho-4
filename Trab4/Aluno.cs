1afefee36955d7f5941810703c7a10f8275fc3f4
Hugo Lourenco <hugomaneira@gmail.com>
2021-06-29 15:05:58 +0100

    commit

---
+++ Trab4/Aluno.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Trab4
+{
+    class Aluno
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public string Morada { get; set; }
+        public int Telemovel { get; set; }
+        public Aluno() { }
+        public Aluno(string n, int i, string m, int t)
+        {
+            Nome = n;
+            Idade = i;
+            Morada = m;
+            Telemovel = t;
+        }
+    }
+}
+
---
+++ Trab4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Trab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Aluno> lista = new();
+            int opcao;
+            do
+            {
+                Console.WriteLine("1 - Inserir aluno");
+                Console.WriteLine("2 - Listar alunos");
+                Console.WriteLine("3 - Sair");
+                Console.WriteLine("Selecione opção:");
+                opcao = Convert.ToInt32(Console.ReadLine());
+                switch (opcao)
+                {
+                    case 1:
+                        //opção 1
+                        Aluno aluno = new Aluno();
+                        Console.WriteLine("Introduza nome do aluno: ");
+                        aluno.Nome = Console.ReadLine();
+                        Console.WriteLine("Introduza idade do aluno: ");
+                        aluno.Idade = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Introduza morada do aluno: ");
+                        aluno.Morada = Console.ReadLine();
+                        Console.WriteLine("Introduza telemóvel do aluno: ");
+                        aluno.Telemovel = Convert.ToInt32(Console.ReadLine());
+                        lista.Add(aluno);
+                        //opção 2
+                        /*Console.WriteLine("Introduza nome do aluno: ");
+                        string nome = Console.ReadLine();
+                        Console.WriteLine("Introduza nome do aluno: ");
+                        int idade = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Introduza nome do aluno: ");
+                        string morada = Console.ReadLine();
+                        Console.WriteLine("Introduza nome do aluno: ");
+                        int telemovel = Convert.ToInt32(Console.ReadLine());
+                        lista.Add(new Aluno(nome, idade, morada, telemovel));*/
+                        break;
+                    case 2:
+                        foreach (Aluno a in lista)
+                        {
+                            Console.WriteLine($"Nome: {a.Nome} - Morada: { a.Morada} - Idade: { a.Idade} - Telemóvel: { a.Telemovel}");
+                        }
+                        break;
+                    case 3:
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        break;
+                }
+            } while (opcao != 3);
+        }
+    }
+}
